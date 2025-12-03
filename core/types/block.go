c5e2e394c3bfbaacfb3c14a9e4027891cbca2b98
Frank Liu <yyc298520@gmail.com>
2025-12-03 21:50:52 +0900

    Expand Block header fields and implement RLP encoding
    - Add more fields to Header (parentHash, txHash, receiptHash, number, gasLimit, gasUsed, baseFee)
    - Update Block to use pointer to Header and slice of Transactions
    - Add ExtBlock for RLP encoding support
    - Implement Block.EncodeRlp method
    - Add basic Encode function to rlp package
    - Add package declaration to rlp/decode.go

--- core/types/block.go
+++ core/types/block.go
@@ -1,9 +1,30 @@
 package types
 
+import "io"
+
 type Header struct {
-	hash string
+	parentHash  string
+	txHash      string
+	receiptHash string
+	number      uint64
+	gasLimit    uint64
+	gasUsed     uint64
+	baseFee     uint64
 }
 
 type Block struct {
-	header Header
+	header *Header
+	txs    []*Transaction
+}
+
+type ExtBlock struct {
+	header *Header
+	txs    []*Transaction
+}
+
+func (b *Block) EncodeRlp(w io.Writer) error {
+	return rlp.Encode(w, &ExtBlock{
+		header: b.header,
+		txs:    b.txs,
+	})
 }
---
+++ rlp/encode.go
@@ -0,0 +1,7 @@
+package rlp
+
+import "io"
+
+func Encode(w io.Writer, val interface{}) error {
+	return nil
+}
