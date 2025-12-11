cc19e2876702c66afafb175cf6f80e2fc8a8b63f
Frank Liu <yyc298520@gmail.com>
2025-12-11 20:37:57 +0900

    Define key-value store interfaces for ethdb
    - Add KeyValueReader interface with Has and Get methods
    - Add KeyValueWriter interface with Put and Delete methods
    - Add KeyValueRangeDeleter interface with DeleteRange method
    - Add KeyValueStore interface combining all three interfaces

--- ethdb/database.go
+++ ethdb/database.go
@@ -1,1 +1,23 @@
 package ethdb
+
+type KeyValueReader interface {
+	Has(key []byte) (bool, error)
+
+	Get(key []byte) ([]byte, error)
+}
+
+type KeyValueWriter interface {
+	Put(key []byte, values []byte) error
+
+	Delete(key []byte) error
+}
+
+type KeyValueRangeDeleter interface {
+	DeleteRange(start, end []byte) error
+}
+
+type KeyValueStore interface {
+	KeyValueReader
+	KeyValueWriter
+	KeyValueRangeDeleter
+}
